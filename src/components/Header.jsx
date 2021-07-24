2a19fb7afe3bfb35c8a89885108bc57c0cc1659d
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-07-24 12:25:02 +0430

    Basic file structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h5>Where in the world?</h5>
+      <div className="theme-mode">
+        <i class="fa fa-moon-o"></i>
+        <span>Dark Mode</span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
