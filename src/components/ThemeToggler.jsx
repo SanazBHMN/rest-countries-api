27d131d92a90a7b4a9f77d5a8be3ee179f460823
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-07-31 11:58:59 +0430

    Use react helmet to switch between themes

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,20 +1,25 @@
 import React, { useContext } from "react";
+import { Helmet } from "react-helmet";
 import ThemeContext from "../libs/Context";
 
 function ThemeToggler() {
   const { theme, setTheme } = useContext(ThemeContext);
   console.log(theme);
 
-  const handleThemeToggle = (e) => {
-    e.preventDefault();
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
+  const bg =
+    theme === "dark"
+      ? "body {background-color: hsl(207, 26%, 17%); color: hsl(0, 0%, 100%)}"
+      : "body {background-color: hsl(0, 0%, 98%); color: hsl(200, 15%, 8%)}";
 
   return (
-    <button className="theme-toggler" onClick={handleThemeToggle}>
-      <span>
-        {theme === "light" ? "🌞" : "🔅"}
-      </span>
+    <button
+      className="theme-toggler"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      <Helmet>
+        <style>{bg}</style>
+        <span>{bg === "light" ? "🌞" : "🔅"}</span>
+      </Helmet>
     </button>
   );
 }
